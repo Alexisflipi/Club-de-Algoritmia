c4e630024ca1cc073d172da10ff1bf1a63f38448
Alexisflipi <roma_alexis@hotmail.com>
2017-01-28 17:38:25 -0600

    Update Mates Gral.cpp

--- Reference 2017/Matematicas/Mates Gral.cpp
+++ Reference 2017/Matematicas/Mates Gral.cpp
@@ -4,14 +4,14 @@ using namespace std;
 typedef long long lld;
 
 lld knight_distance(lld x1, lld y1, lld x2, lld y2) {
-    lld x = abs(x2 - x1), y = abs(y2 - y1);    
-    if (x > y) swap(x, y);
-    assert(x <= y);
-    if (x == 0 and y == 1) return 3;
-    if (x == 2 and y == 2) return 4;
-    lld d = max((y + 1) / 2, (x + y + 2) / 3);
-    if (d % 2 != (x + y) % 2) d++;
-    return d;
+  lld x = abs(x2 - x1), y = abs(y2 - y1);    
+  if (x > y) swap(x, y);
+  assert(x <= y);
+  if (x == 0 and y == 1) return 3;
+  if (x == 2 and y == 2) return 4;
+  lld d = max((y + 1) / 2, (x + y + 2) / 3);
+  if (d % 2 != (x + y) % 2) d++;
+  return d;
 }
 //Bitmask: Da todas las posibles configuraciones
 //Complejidad: 2^n
