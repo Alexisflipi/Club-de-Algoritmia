883757488e4a283fe73bfead06ce672aa4917298
Alexisflipi <roma_alexis@hotmail.com>
2017-01-28 23:19:23 -0600

    Create Union-Find.cpp

---
+++ Reference 2017/Estructuras de datos/Union-Find.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+struct UnionFind{
+  int n, sets;
+  vi p, r, s;
+  UnionFind(int N) : n(N), sets(N), 
+  r(N), s(N, 1), p(N) { 
+    while (--N) p[N] = N; 
+  }
+  //Find, utiliza compresión de caminos
+  int Find(int i) {
+    return (i == p[i]) ? i : (p[i] = Find(p[i]));
+  }
+  //Determina si están en la misma componente
+  bool isSameSet(int i, int j) {
+    return (Find(i) == Find(j));
+  }
+  //Union, utiliza rango para un arbol corto y sea O(~1)
+  void Union(int i, int j) {
+    int x = Find(i), y = Find(j);
+    if (x == y) return; else sets--;
+    (r[x] > r[y]) ? p[y] = x, s[y] += s[x] 
+                  : p[x] = y, s[x] += s[y];
+    if (r[x] == r[y]) r[y]++;
+  }
+  //Retorna el tamaño del conjunto del nodo i
+  int sizeSet(int i) {
+    return s[Find(i)];
+  }
+  //Retorna el número de componentes conexas
+  int numSet() {
+    return sets;
+  }
+};
