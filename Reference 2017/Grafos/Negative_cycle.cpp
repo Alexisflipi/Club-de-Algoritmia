d315c14102646afa4f37ebc0fb2beba4d81c95dd
Alexisflipi <roma_alexis@hotmail.com>
2017-10-19 01:05:46 -0500

    Update Negative_cycle.cpp

--- Reference 2017/Grafos/Negative_cycle.cpp
+++ Reference 2017/Grafos/Negative_cycle.cpp
@@ -1,28 +1,3 @@
- vector<Costo> dist; // <- Resultado
-
-bool BellmanFerrari(int s) {
-  queue<int> q; 
-  vector<bool> en_cola(n);
-  vector<int> procesado(n);
-  dist = vector<Costo>(n, INF);
-  q.push(s), dist[s] = 0;
-
-  while (!q.empty()) {
-    int u = q.front();
-    q.pop(), en_cola[u] = false;
-    if (++procesado[u] == n) return true;
-    for (CostoNodo arista : ady[u]) {
-      int v = arista.second;
-      Costo p = arista.first;
-      if (dist[u] + p < dist[v]) {
-        if (!en_cola[v]) q.push(v);
-        dist[v] = dist[u] + p;
-        en_cola[v] = true;
-      }
-    }
-  }
-  return false;
-}
 
 bool BellmanFord(int s) {
   //v number of vertices
