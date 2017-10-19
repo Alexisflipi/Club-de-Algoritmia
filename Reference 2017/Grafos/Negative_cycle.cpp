b2156bc879594ee4fd7b1fdd19332ae87dd91d9e
Alexisflipi <roma_alexis@hotmail.com>
2017-10-19 00:57:56 -0500

    Update Negative_cycle.cpp

--- Reference 2017/Grafos/Negative_cycle.cpp
+++ Reference 2017/Grafos/Negative_cycle.cpp
@@ -23,3 +23,24 @@ bool BellmanFerrari(int s) {
   }
   return false;
 }
+
+bool BellmanFord(int s) {
+  //v number of vertices
+  vi dist(v, oo); dist[s] = 0;
+  bool hasNegaCycle = 0;
+  for (int i = 0; i < v; i++) {
+    bool modi = 0;
+    for (int u = 0; u < v; u++) {
+      for (auto &v : ady[u]) {
+        if (dist[u] + v.second < dist[v.first]) {
+          dist[v.first] = dist[u] + v.second;
+          modi = 1;
+        }
+      }
+    }
+    if (!modi) break;
+    if (modi && i == v - 1) hasNegaCycle = 1;
+  }
+  return hasNegaCycle;
+}
+
